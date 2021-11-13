9522fc630c22b03c21100d45a999b4d81af95161
alonhi <93863138+alonhi@users.noreply.github.com>
2021-11-13 11:56:22 +0200

    test rebase 2

--- TestGit/ContentView.swift
+++ TestGit/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             Text("test develpment")
             Text("test rebase")
             Text("test rebase")
+            Text("test")
         }
         
     }

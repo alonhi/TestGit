1495ee219d55f53db179c63ef68afe6c12567ea5
alonhi <93863138+alonhi@users.noreply.github.com>
2021-11-13 11:41:33 +0200

    first bug fix commit

---
+++ TestGit/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  TestGit
+//
+//  Created by Andrii Cheberiako on 13.11.2021.
+//
+
+import Foundation
+
+class cool {
+    func cool() {
+        print("cooollll")
+    }
+}

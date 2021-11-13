fecad455cdcef4b43340bd7d9532551c7bd1c227
alonhi <93863138+alonhi@users.noreply.github.com>
2021-11-13 11:34:33 +0200

    first commit

---
+++ TestGit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TestGit
+//
+//  Created by Andrii Cheberiako on 13.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .font(.body)
+        Text("test 1")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Andrii Cheberiako on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
